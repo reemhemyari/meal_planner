1b641cc9f7550fd245b55ad345a4bc4862cab612
Reem Al-Hemyari <reem.hemyari@gmail.com>
2020-06-08 17:16:15 +0100

    Chooses random options from a list: meals

--- choose_meal.py
+++ choose_meal.py
@@ -1,17 +1,10 @@
 import random
+from get_meals_from_file import get_stored_meals
 
-meals = ["Chicken Fajitas", "Spaghetti", "Sweet Potato and Beans", "Salmon Pasta", "Fish Tacos",
-         "Beef Tacos", "Chicken and Carrots", "Chicken Curry", "Salmon with Veg", "Roast Chicken",
-         "Roast Beef", "Eat out", "Chicken and Leak pie", "Lentil Spinach Curry", "Chicken Stir-fry",
-         "Veg Stir-fry", "Pasta Bake", "Home-made Pizza", "Beef Hot Pot", "Fried Chicken"]
+meals = get_stored_meals()
 
 
-class ChooseMeal:
-    def choose_meals(self, num_options: int) -> str:
-        for _ in range(0, num_options, 1):
-            print(random.choice(meals))
-        return random.choice(meals)
-
-    if __name__ == '__main__':
-        num_of_options = int(input("Enter a number of options you would like to be generated: "))
-        choose_meals(None, num_of_options)
+def choose_meals(num_options) -> str:
+    for _ in range(0, num_options, 1):
+        print(random.choice(meals))
+    return random.choice(meals)

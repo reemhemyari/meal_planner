0f633ff9bc7d7212ef4cc0221ca1537ba3288f78
Louis Burton <louis.burton@winnowsolutions.com>
2020-06-12 17:21:51 +0100

    Introduce Makefile and structure to project

--- tests/test_get_meals_from_file.py
+++ tests/test_get_meals_from_file.py
@@ -1,20 +1,16 @@
-from get_meals_from_file import get_stored_meals
+from meal_planner import get_meals_from_file
 
-meals = get_stored_meals()
+meals = get_meals_from_file.get_stored_meals()
 
 
 def test_no_num_there():
     for lines in meals:
         [(line.strip()) for line in meals]
         assert any(character.isdigit() for character in lines) is False
-        # The isdigit() methods returns “True” if all characters in the string are digits,
+        # The isdigit() methods returns “True”
+        # if all characters in the string are digits,
         # Otherwise, It returns “False”. Syntax : string.
 
 
 def test_get_stored_meals_is_not_empty():
     assert len(meals) != 0
-
-
-if __name__ == '__main__':
-    test_get_stored_meals_is_not_empty()
-    test_no_num_there()

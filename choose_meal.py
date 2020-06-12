5109994c37e31406fac1605c913a34ce0083a0f1
Reem Al-Hemyari <reem.hemyari@gmail.com>
2020-06-12 12:18:30 +0100

    Changed the code so it doesn't give you an answer more than once while the program is running.

--- choose_meal.py
+++ choose_meal.py
@@ -1,12 +1,22 @@
 import random
+from typing import List
 from get_meals_from_file import get_stored_meals
 
 meals = get_stored_meals()
+meals_copy = meals
 
 
-def choose_meals(num_options) -> str:
-    meal = meals[:]  # make a copy of the input list
-    random.shuffle(meal)
-    for i in range(num_options):
-        print(meal.pop())
-    return random.choice(meals)
+def choose_meals(num_options) -> List[str]:
+    chosen_meals = []
+    if len(meals_copy) > 0:
+        for _ in range(0, num_options, 1):
+
+            choice = random.choice(meals_copy)
+            if choice not in chosen_meals:
+                chosen_meals.append(choice)
+                meals_copy.remove(choice)
+            else:
+                choice2 = random.choice(meals_copy)
+                chosen_meals.append(choice2)
+
+    return chosen_meals
--- meal_planner.py
+++ meal_planner.py
@@ -1,29 +1,40 @@
+from get_meals_from_file import get_stored_meals
+from choose_meal import choose_meals
+
 
 def main():
-    from get_meals_from_file import get_stored_meals
-    from choose_meal import choose_meals
-
     meals = get_stored_meals()
 
     num_options = int(input("Enter a number of options you would like to be generated: "))
 
     # if statement ensures that the user chooses to generate at least one option, otherwise stopping the program
     if num_options < 1:
+        print("")
         print("Error - Number must be 1 or larger")
         print("Program stopped")
     elif num_options > len(meals):
+        print("")
         print("Error - There aren't that many options available")
         print("Program stopped")
+    elif type(num_options) is not int:
+        print("Error - Must be an integer")
     else:
-        choose_meals(num_options)
+        chosen_meals = choose_meals(num_options)
+        print(chosen_meals)
 
         # the while loop allows the user to regenerate options
         new_options = "y"
         while new_options == "y":
             new_options = str(input("Would you like to generate new options? (y/n): "))
             if new_options == "y":
-                choose_meals(num_options)
+                regenerated_choice = choose_meals(num_options)
+                if len(regenerated_choice) > 0:
+                    print(regenerated_choice)
+                else:
+                    print("Error - List empty, no options left")
+                    break
             else:
+                print("")
                 print("Program stopped")
 
 

76c065b27efc7c9b033272ccadaab6896719bd5e
Reem Al-Hemyari <reem.hemyari@gmail.com>
2020-06-15 17:51:45 +0100

    removed redundant code

--- meal_planner/choose_meal.py
+++ meal_planner/choose_meal.py
@@ -3,20 +3,19 @@ from typing import List
 from meal_planner import get_meals_from_file
 
 meals = get_meals_from_file.get_stored_meals()
-meals_copy = meals
 
 
 def choose_meals(num_options: int) -> List[str]:
     chosen_meals = []
-    if len(meals_copy) > 0:
+    if len(meals) > 0:
         for _ in range(0, num_options, 1):
 
-            choice = random.choice(meals_copy)
+            choice = random.choice(meals)
             if choice not in chosen_meals:
                 chosen_meals.append(choice)
-                meals_copy.remove(choice)
+                meals.remove(choice)
             else:
-                choice2 = random.choice(meals_copy)
+                choice2 = random.choice(meals)
                 chosen_meals.append(choice2)
 
     return chosen_meals

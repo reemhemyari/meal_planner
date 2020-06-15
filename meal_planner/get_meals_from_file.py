57199c7351a3c2cfcbf19a7fd3128a9dc8cf3f5b
Reem Al-Hemyari <reem.hemyari@gmail.com>
2020-06-15 17:55:02 +0100

    changed it so it works

--- meal_planner/get_meals_from_file.py
+++ meal_planner/get_meals_from_file.py
@@ -2,5 +2,5 @@ from typing import List
 
 
 def get_stored_meals() -> List[str]:
-    with open("recipes.txt", "r") as file:
+    with open("../recipes.txt", "r") as file:
         return [(line.strip()) for line in file]

2be7477a85ea4ab0ce937f135fde78da67d2ff7b
Reem Al-Hemyari <reem.hemyari@gmail.com>
2020-06-15 17:53:38 +0100

    changed test so it actually tests the file it's supposed to

--- tests/test_get_meals_from_file.py
+++ tests/test_get_meals_from_file.py
@@ -1,9 +1,8 @@
 from meal_planner import get_meals_from_file
 
-meals = get_meals_from_file.get_stored_meals()
-
 
 def test_no_num_there():
+    meals = get_meals_from_file.get_stored_meals()
     for lines in meals:
         [(line.strip()) for line in meals]
         assert any(character.isdigit() for character in lines) is False
@@ -13,4 +12,5 @@ def test_no_num_there():
 
 
 def test_get_stored_meals_is_not_empty():
+    meals = get_meals_from_file.get_stored_meals()
     assert len(meals) != 0
